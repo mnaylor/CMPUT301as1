7e4185e0dbcaed8871af9bf964b098a194d8a28a
Michelle Naylor <michnaylor@gmail.com>
2013-09-16 16:45:32 -0600

    note class added

---
+++ src/mnaylor/as1/NewNoteActivity.java
@@ -0,0 +1,74 @@
+/** Based on Android tutorial published under Creative Commons
+* http://developer.android.com/training/index.html
+* 
+* Published under GPL-V2
+* http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+package mnaylor.as1;
+
+import note.Note;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class NewNoteActivity extends Activity {
+	
+	@SuppressLint("New API")
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+     // Get the subject from the intent
+     Intent intent = getIntent();
+     String subject = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+     Note new_note = new Note(subject);
+
+     // Set the text view as the activity layout
+     setContentView(R.layout.activity_display_note);
+
+     // Display subject and date
+     EditText subject_line = (EditText) findViewById(R.id.subject);
+     subject_line.setText(new_note.subject);
+     
+     EditText date_line = (EditText) findViewById(R.id.date);
+     date_line.setText(new_note.note_date);
+     
+     // TODO: add note to database
+     
+     
+     
+     // Make sure we're running on Honeycomb or higher to use ActionBar APIs
+     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+    	 // Show the Up button in the action bar.
+    	 getActionBar().setDisplayHomeAsUpEnabled(true);
+       }	
+    }
+	
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
---
+++ src/note/Note.java
@@ -0,0 +1,27 @@
+package note;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Note {
+	public String subject;
+	public String contents;
+	public String note_date;
+	
+	/* Create a note by declaring a subject*/
+	public Note(String new_subject) {
+		subject = new_subject;
+		
+		SimpleDateFormat sdf = new SimpleDateFormat( "yyyy/MM/dd" ); 
+		note_date = sdf.format(new Date());
+	}
+	
+	public void set_contents(String new_contents) {
+		contents = new_contents;
+	}
+	
+	/* Use to change the date */
+	public void set_date() {
+
+	}
+}
